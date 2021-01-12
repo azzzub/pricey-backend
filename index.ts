4615aa15c86bca575868a3edfa1940ca7b8a4813
zub <zub@pop-os.localdomain>
2021-01-12 14:20:48 +0700

    vercel config

--- index.ts
+++ index.ts
@@ -13,9 +13,17 @@ app.use(express.json())
 app.use(cors())
 app.use(helmet())
 
+app.get('/', (_req, res) => {
+  res.json({ message: 'hello there!' })
+})
+
 app.get('/tokped/simple', TokpedFetch)
 app.get('/shopee/simple', ShopeeFetch)
 app.get('/ecom/simple', EcomFetch)
+
+app.get('*', (_req, res) => {
+  res.status(404).json({ message: 'where u wanna go?' })
+})
 
 const PORT = process.env.PORT || 5000
 
